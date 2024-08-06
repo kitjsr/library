cac7f7f560070284bf75103c6e2b8e67feea9a29
Anushruti Mahato <anushruti5882@gmail.com>
2024-08-06 16:08:06 +0530

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
 
 export const routes: Routes = [
+    {path:'',component:HomeComponent},
     {path:'home',component:HomeComponent},
+    {path:'sideNavbar',component:SideNavbarComponent},
+    {path:'**',component:HomeComponent},
 ];
